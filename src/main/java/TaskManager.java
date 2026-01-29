e2edb5776fd605e12dcb53dab10dc5cc58fae93c
Delwyn <178507206+nywled@users.noreply.github.com>
2026-01-29 14:14:59 +0800

    Storage: Refactor Storage.java to TaskManager.java

---
+++ src/main/java/TaskManager.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+import storage.Storage;
+
+import tasks.Task;
+
+public class TaskManager {
+    private final Storage storage;
+    private final ArrayList<Task> taskList;
+
+    public TaskManager() {
+        this.storage = new Storage();
+        this.taskList = storage.loadTasks(); // load from file into memory
+    }
+
+    public void addTask(Task task) {
+        taskList.add(task);
+        storage.saveTasks(taskList); //Autosave after every task add
+    }
+
+    public Task removeTask(int i) {
+        Task removedTask = taskList.remove(i);
+        storage.saveTasks(taskList); //Autosave after every task add
+        return removedTask;
+    }
+
+    public Task getTask(int i){
+        return taskList.get(i);
+    }
+
+    public int getTaskListSize() {
+        return taskList.size();
+    }
+
+    public void save() {
+        storage.saveTasks(taskList);
+    }
+}
