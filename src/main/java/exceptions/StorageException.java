05b697e4ee4fb803c2f81591e748b9d54b0e801b
Delwyn <178507206+nywled@users.noreply.github.com>
2026-01-29 14:16:18 +0800

    Storage: Add exception handling

---
+++ src/main/java/exceptions/StorageException.java
@@ -0,0 +1,12 @@
+/**
+ * Exception class to handle storage exceptions
+ */
+package exceptions;
+
+public class StorageException extends MomoException {
+
+    public StorageException(String message) {
+        super(message);
+    }
+
+}
